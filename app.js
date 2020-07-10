086ab99831d491d6c170fa879ec422865a3c776a
Hussnain Quresshi <hussnain77133@gmail.com>
2020-07-11 02:38:29 +0500

    Error removed

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ require('express-async-errors');
 const express = require('express');
 const winston = require('winston');
 const app = express();
+const path = require('path');
 
 require('./Startup/db')(); //Database connection
 require('./Startup/errorhandler')(app); //error handling if for anyreason something went wrong in our code :)
 app.use(express.json());
 app.use('/api', require('./routes/index'));
+
+app.use(express.static(path.join(__dirname, 'Frontend/build')));
 app.get('*', function (req, res) {
+  console.log('m here');
   res.sendFile(path.join(__dirname, 'Frontend/build', 'index.html'));
 });
 const port = process.env.PORT || 5000;
