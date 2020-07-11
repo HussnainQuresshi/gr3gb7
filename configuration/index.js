9b471b65335dd0d2e50fdf38c2af9151ef99babb
Hussnain Quresshi <hussnain77133@gmail.com>
2020-07-11 16:40:25 +0500

    made responsive

--- configuration/index.js
+++ configuration/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // db: 'mongodb://localhost:27017/test',
-  db:
-    'mongodb+srv://Hussnain:h4ck3r77133@mongodb-nbww5.mongodb.net/gr3gb7?retryWrites=true&w=majority',
+  db: 'mongodb://localhost:27017/test',
+  // db:
+  //   'mongodb+srv://Hussnain:h4ck3r77133@mongodb-nbww5.mongodb.net/gr3gb7?retryWrites=true&w=majority',
 };
